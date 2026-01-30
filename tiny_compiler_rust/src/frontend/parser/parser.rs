53d8f04d73f9a134a660ec2446f7553a987baf9c
Kenny Kim <caspi4646@gmail.com>
2026-01-30 12:25:37 -0800

    added new function for parser and the logic for each function

--- tiny_compiler_rust/src/frontend/parser/parser.rs
+++ tiny_compiler_rust/src/frontend/parser/parser.rs
@@ -5,30 +5,70 @@ use crate::frontend::fsm::{
 
 pub struct Parser {
     tokens: Vec<Token>,
+    cur_token: Toekn,
 }
 
 impl Parser {
     fn new(input: String) -> Self {
         let tokenizer = Tokenizer::new(input);
         tokenizer.generate_token();
+        if tokens.len() < 0 {
+            panic!("No User Input/Toeken");
+        }
         Self {
             tokens: tokenizer.get_token(),
+            cur_token: tokens[0],
         }
     }
-    fn factor(&self) {}
-    fn term(&self) {}
-    fn expression(&self) {}
-    fn relation(&self) {}
-    fn assignment(&self) {}
-    fn funcCall(&self) {}
-    fn ifStatement(&self) {}
-    fn whileStatement(&self) {}
-    fn returnStatement(&self) {}
-    fn statement(&self) {}
-    fn statSequence(&self) {}
-    fn varDecl(&self) {}
-    fn funcDecl(&self) {}
-    fn formalParam(&self) {}
-    fn funcBody(&self) {}
-    fn computation(&self) {}
+    fn factor(&self) {
+        // ident | number | "(" expression ")" | funcCall
+    }
+    fn term(&self) {
+        // factor { ("*" | "/") factor }
+    }
+    fn expression(&self) {
+        // term { ("+" | "-") terrm }
+    }
+    fn relation(&self) {
+        // expression relOp expression
+    }
+    fn assignment(&self) {
+        // "let" ident "<-"  expression
+    }
+    fn funcCall(&self) {
+        // "call" ident [ "(" [expression {"," expression}] ")"]
+    }
+    fn ifStatement(&self) {
+        // "if" relation "then" statSequence ["else" statSequence] "fi"
+        // new block: if, then, else, fi
+    }
+    fn whileStatement(&self) {
+        // "while" relaton "do" StatSequence "od"
+        // new block: while, then
+    }
+    fn returnStatement(&self) {
+        // assignment | funcCall | ifStatement | whileStatement | returnStatement
+    }
+    fn statement(&self) {
+        // statement {";" statement } [";"]
+    }
+    fn statSequence(&self) {
+        // statement {";" statement } [";"]
+    }
+    fn varDecl(&self) {
+        // ["void"] "function" ident formalParam ";" funcBody ";"
+    }
+    fn funcDecl(&self) {
+        // "(" [ident { "," ident }] ")"
+        // new block
+    }
+    fn formalParam(&self) {
+        // "( [ident {"," ident}] ")"
+    }
+    fn funcBody(&self) {
+        // [varDecl] "{" [statSequence] "}"
+    }
+    fn computation(&self) {
+        // "main" [varDecl] {funcDecl} "{" statSequence "}" "."
+    }
 }
