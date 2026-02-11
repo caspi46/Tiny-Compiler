b5c47f8453f71fd6a33ffca0a28e64bbfa31e65c
Kenny Kim <caspi46@naver.com>
2026-02-10 18:53:21 -0800

    deleted previous work (tiny_compiler folder)

--- tiny_compiler/target/debug/deps/tiny_compiler-0db7b1610d1eb7d5.d
+++
@@ -1,5 +0,0 @@
-/Users/kennykim/Documents/Tiny-Compiler/tiny_compiler/target/debug/deps/tiny_compiler-0db7b1610d1eb7d5.d: src/main.rs
-
-/Users/kennykim/Documents/Tiny-Compiler/tiny_compiler/target/debug/deps/libtiny_compiler-0db7b1610d1eb7d5.rmeta: src/main.rs
-
-src/main.rs:
