382a75e01b3d0b98ddb0f4bf993f7a537c72bcfc
Kenny Kim <caspi4646@gmail.com>
2026-02-12 15:08:54 -0800

    new feature: instruction storage for Add, Sub, Mul, Div

--- tiny_compiler_rust/src/frontend/frontend.md
+++ tiny_compiler_rust/src/frontend/frontend.md
@@ -8,11 +8,20 @@
 ### Parser (WIP):
 - Parser: (WIP)
     - Role: Parser thing. 
+    - Current stage: skeleton 
+    - varDecl: adding variable in variables' hash map (ex: var1 : 0 as default)
+- Return Values for functions: 
+    - The return value type will be i32 because it returns the inst #.
+        - TODO: Need to figure out which inst# should be return
     
 
 ### Operator (WIP): TODO: Test! 
 - Operator: (WIP) 
     - Role: Define Operator types 
+- Inst
+- InstStorage: Instruction Storage for OP 
+    - This is for optimization: common subexpression elimination (for now)
+    - I'll think about the copy propagation later 
 
 ## Performance: (WIP: STUDY!!!)
 ### [Copy propagation](https://en.wikipedia.org/wiki/Copy_propagation):
---
+++ tiny_compiler_rust/src/frontend/operators/inst_storage.rs
@@ -0,0 +1,81 @@
+use crate::frontend::operators::inst::Inst;
+use crate::frontend::operators::operator::Operator;
+use std::collections::HashMap;
+// For optimization
+// each vector contains the each Inst type instructions
+// Every time new inst is created, it is added into the storage.
+
+// My thought process lol
+// The add function should be called before inserting it into block or insts
+// Since this is only for optimizing so far, this is not used for the actual Inst collection yet
+// But, depending on the situation, it could be used as collection
+// But, the collection is only for Add, Sub, Div, and Mul, so creating extra collection is unavoidable.
+pub struct InstStorage {
+    adds: HashMap<Operator, i32>,
+    subs: HashMap<Operator, i32>,
+    muls: HashMap<Operator, i32>,
+    divs: HashMap<Operator, i32>,
+}
+
+impl InstStorage {
+    pub fn new() -> Self {
+        Self {
+            adds: HashMap::new(),
+            subs: HashMap::new(),
+            muls: HashMap::new(),
+            divs: HashMap::new(),
+        }
+    }
+
+    fn add_adds(&mut self, new_add: Operator, inst_num: i32) -> i32 {
+        if matches!(new_add, Operator::Add(_, _)) {
+            panic!("Error: Failed to add new add instruction in storage");
+        }
+        if !self.adds.contains_key(&new_add) {
+            self.adds.insert(new_add.clone(), inst_num);
+        }
+        match self.adds.get(&new_add) {
+            Some(&i) => i,
+            _ => panic!("Error: Failed to add new add operator in storage"),
+        }
+    }
+
+    fn add_subs(&mut self, new_sub: Operator, inst_num: i32) -> i32 {
+        if matches!(new_sub, Operator::Sub(_, _)) {
+            panic!("Error: Failed to add new sub instruction in storage");
+        }
+        if !self.subs.contains_key(&new_sub) {
+            self.subs.insert(new_sub.clone(), inst_num);
+        }
+        match self.subs.get(&new_sub) {
+            Some(&i) => i,
+            _ => panic!("Error: Failed to add new sub operator in storage"),
+        }
+    }
+
+    fn add_divs(&mut self, new_div: Operator, inst_num: i32) -> i32 {
+        if matches!(new_div, Operator::Div(_, _)) {
+            panic!("Error: Failed to add new div instruction in storage");
+        }
+        if !self.divs.contains_key(&new_div) {
+            self.divs.insert(new_div.clone(), inst_num);
+        }
+        match self.subs.get(&new_div) {
+            Some(&i) => i,
+            _ => panic!("Error: Failed to add new div operator in storage"),
+        }
+    }
+
+    fn add_muls(&mut self, new_mul: Operator, inst_num: i32) -> i32 {
+        if matches!(new_mul, Operator::Mul(_, _)) {
+            panic!("Error: Failed to add new mul instruction in storage");
+        }
+        if !self.muls.contains_key(&new_mul) {
+            self.muls.insert(new_mul.clone(), inst_num);
+        }
+        match self.subs.get(&new_mul) {
+            Some(&i) => i,
+            _ => panic!("Error: Failed to add new mul operator in storage"),
+        }
+    }
+}
